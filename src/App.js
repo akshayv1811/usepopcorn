04c03f48a762a419a39de4bfe97088ebe783d1eb
akshayv1811 <akshayv1811@gmail.com>
2024-09-03 23:40:00 +0530

    App component temporaily commented and Star Rating reusable component is built and tested

--- src/App.js
+++ src/App.js
@@ -125,7 +125,7 @@ function Box({ children }) {
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children} 
+      {isOpen && children}
     </div>
   );
 }
